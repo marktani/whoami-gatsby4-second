7cfeaaf74382063e83565bf179b60f3564fd3d65
Nilan Marktanner <nilan.marktanner@gmail.com>
2022-05-01 19:48:38 +0200

    adjust not 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,75 @@
 import Layout from '../components/Layout'
+import Sidebar from '../components/Sidebar'
 import Seo from '../components/seo'
+import { graphql } from 'gatsby'
 import * as React from 'react'
 
-const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+class NotFoundPage extends React.Component {
+  render() {
+    const { title } = this.props.data.site.siteMetadata
+
+    return (
+      <Layout>
+        <Seo title={title} />
+        <Sidebar {...this.props} />
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    )
+  }
+}
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+  query NotFoundPageQuery {
+    site {
+      siteMetadata {
+        title
+        subtitle
+        copyright
+        services {
+          label
+          path
+        }
+        projects {
+          label
+          path
+        }
+        about {
+          label
+          path
+        }
+        author {
+          name
+          email
+          twitter
+          github
+          medium
+        }
+      }
+    }
+    allMarkdownRemark(
+      limit: 1000
+      filter: { frontmatter: { layout: { eq: "post" }, draft: { ne: true } } }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            category
+            description
+            path
+          }
+          fields {
+            tagSlugs
+            slug
+            categorySlug
+          }
+        }
+      }
+    }
+  }
+`
